57b3d8c4cb456fe4cfdabcb4cd516fee49b3828c
Greg Larsen <greg.larsen@hp.com>
2015-08-13 15:31:09 +0200

    first commit

--- jenkins/inject_date.sh
+++ jenkins/inject_date.sh
@@ -6,12 +6,9 @@ for i in `find . -name "*.dita"`
 do
 
     j=`echo $i | sed 's|\.dita$|\.html|'`
-
     fullpath=`echo $j | sed 's|\.\/|./out/webhelp/|'`
-
     DATE=`git log -1 --date=short --pretty=format:%ad $i`
-
     PRETTYDATE=`date -d$DATE +'%d %b %Y'`
-
     sed -i "s|<\/h1>|</h1>Last updated: $PRETTYDATE|" $fullpath    
- done
+
+done

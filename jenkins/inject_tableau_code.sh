63c480b3b8355b40db6d5298488d4336aa9d8b58
Greg Larsen <greg.larsen@hp.com>
2015-08-18 11:13:29 +0200

    correct character error

--- jenkins/inject_tableau_code.sh
+++ jenkins/inject_tableau_code.sh
@@ -10,14 +10,14 @@ do
 
 
 
-   METRIC_CODE1="<!-ÎíÎñ Begin Analytics Code -->"
+   METRIC_CODE1="<!-- Begin Analytics Code -->"
    METRIC_CODE2="<script language='JavaScript'>"
    METRIC_CODE3="var hpmmd=window.hpmmd||{type: 'Cleansheet Wash', page:{events:[]}, product:{},user:{},legacy:{},promo:{}};"
    METRIC_CODE4="hpmmd.page.name='$TITLE';"
    METRIC_CODE5="</script>"
    METRIC_CODE6="<script language=\"JavaScript\" type=\"text/javascript\" src=\"https://ssl.www8.hp.com/h10000/cma/ng/lib/bootstrap/metrics.js\">"
    METRIC_CODE7="</script>"
-   METRIC_CODE8="<!ÎíÎñ- End Analytics Code -->"
+   METRIC_CODE8="<!-- End Analytics Code -->"
 
    echo metric code $METRIC_CODE1
 

7971ce4f5d47146a35ccd849f5647d71f843e31b
Greg Larsen <greg.larsen@hp.com>
2015-08-18 14:18:22 +0200

    first commit

---
+++ jenkins/insert_disclaimer.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+echo ===start inject_disclaimer===
+DISCLAIMER=`cat disclaimer_snippet` || true
+for i in `find ./out/webhelp -name *.html`
+do
+   echo "inject disclaimer into $i"
+   sed -i "s|\([^>]\)</h1>|\1</h1> $DISCLAIMER|" $i
+
+done
+
+echo ===stop inject_disclaimer===

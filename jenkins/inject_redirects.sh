4a2ec1ab56aa5e307af9a6809dfce026f4e7091a
Greg Larsen <greg.larsen@hp.com>
2015-08-19 14:18:54 +0200

    fixes

--- jenkins/inject_redirects.sh
+++ jenkins/inject_redirects.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo start inject redirects
 
+grep -v "#" ./ServerArtifacts/inter-helpset-redirects.txt > inter-helpset-redirects.tmp 
   
 while read -r FROM TO; do
   echo $FROM $TO;
   REDIRECT="if (dynamicURL == '$FROM') {window.location.href = '#$TO';} else"
   echo $EDIRECT
-done < `grep -v "#" ./ServerArtifacts/inter-helpset-redirects.txt;`
+done < inter-helpset-redirects.tmp 
 
 echo stop inject redirects
 #sed –i  "s| function loadIframe(dynamicURL) {| function loadIframe(dynamicURL) { $REDIRECT|"  ./oxygen-webhelp/resources/skins/desktop/toc_driver.js

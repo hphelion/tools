7b1101d1bbe31ed9aa77025d7f37ff5ceafae479
Greg Larsen <greg.larsen@hp.com>
2015-08-20 18:42:36 +0200

    changes for ditaval test

--- jenkins/oxygen-webhelp-build.sh
+++ jenkins/oxygen-webhelp-build.sh
@@ -118,10 +118,10 @@ DITAMAP_FILE=docs.hpcloud.com.ditamap
 
 # The name of the DITAVAL input filter file 
 DITAVAL_FILE=my_ditaval.ditaval
-echo where am I
+ 
 pwd
 # The path of the directory of the DITAVAL input filter file
-DITAVAL_DIR=/usr/local/OxygenXMLDeveloper16/samples/dita
+DITAVAL_DIR=./documentation/ServerArtifacts/
 
 
 "java"\

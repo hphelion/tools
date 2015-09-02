4d80fe82503855dfe51c452face9ad1bc5494b82
Greg Larsen <greg.larsen@hp.com>
2015-09-02 13:07:12 +0200

    first commit

---
+++ jenkins/hipchat-notification.sh
@@ -0,0 +1,24 @@
+!#/bin/bash
+#Send notification to hipchat
+
+COLOR=$1 #valid values are red, green, or yellow
+ROOM=$2
+MESSAGE=$3
+EXIT=$4 #valid values are 1 or 0
+
+ 
+curl \
+	--header "Authorization: Bearer zKuxF5Bt5H9dpNysOSf8nRPw2GbT41f3vAS5jKSI" \
+	--header "Content-Type: application/json" \
+	--request 'POST' \
+	--data @- \
+	https://hipchat.hpcloud.net/v2/room/${ROOM}/notification <<EOP
+{
+	"color":"$COLOR",
+	"notify":false,
+	"message":"$MESSAGE  You can see the list of issues with your Jenkins login info (<a href=\"$CONSOLE\">here</a>.)",
+	"message_format":"html"
+}
+EOP
+
+         exit "$EXIT"

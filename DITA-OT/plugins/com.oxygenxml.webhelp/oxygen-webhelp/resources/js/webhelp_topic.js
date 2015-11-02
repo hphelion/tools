3298333e9e73729561e66e8b0ced7467bb5be180
Greg Larsen <greg.larsen@hpe.com>
2015-11-02 21:02:59 +0100

    add para breaks in feedback message body

--- DITA-OT/plugins/com.oxygenxml.webhelp/oxygen-webhelp/resources/js/webhelp_topic.js
+++ DITA-OT/plugins/com.oxygenxml.webhelp/oxygen-webhelp/resources/js/webhelp_topic.js
@@ -11,7 +11,7 @@ available in the base directory of this Oxygen Webhelp plugin.
 function feedback(){
 
 var topic=document.getElementsByTagName("H1")[0].innerHTML ;
-document.getElementById("feedback").innerHTML = "<a href=\"mailto:heliondocs@hpe.com&subject=Feedback on '"+topic+"'&body=Thank you for providing the Helion Documentation Team with feedback about this topic!  Please replace this text with your feedback.%0D%0ALeave the title unchanged, because this will make it clear to us what topic you are providing feedback about.  If you need support, please contact your HPE Support representative.%0D%0AThanks for helping us improve the documentation!\">Feedback to the Helion Docs Team</a>  ";
+document.getElementById("feedback").innerHTML = "<a href=\"mailto:heliondocs@hpe.com&subject=Feedback on '"+topic+"'&body=Thank you for providing the Helion Documentation Team with feedback about this topic!  Please replace this text with your feedback.%0D%0A %0D%0ALeave the title unchanged, because this will make it clear to us what topic you are providing feedback about.  If you need support, please contact your HPE Support representative.%0D%0A %0D%0AThanks for helping us improve the documentation!\">Feedback to the Helion Docs Team</a>  ";
 
 }
 

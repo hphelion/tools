f12368fed2de979210f3e02d244e282a22b2247e
Greg Larsen <greg.larsen@hpe.com>
2015-12-15 10:26:42 +0100

    first commit

---
+++ jenkins/set_publish_branch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+git checkout master
+git pull
+
+echo "
+<html>
+<body>
+<p>The $1 branch currently selected to push to production is:</p>
+
+<p><b>
+
+$BRANCH_TO_PUBLISH
+
+</b>
+</p>
+
+</body>
+</html>
+
+"> ./jenkins/${1}.publish.branch.html
+
+git add .
+git commit -m "A jenkins job changed the $1 publish branch to $BRANCH_TO_PUBLISH"
+git push

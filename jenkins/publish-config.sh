357e1e5deecfca70bbdabddf087f6c21c72d1abb
helion-jenkins <helion-jenkins@hp.com>
2015-12-15 11:58:53 +0000

    A jenkins job changed the carrier.grade publish branch to test

--- jenkins/publish-config.sh
+++ jenkins/publish-config.sh
@@ -1,5 +1,5 @@
 devplat_docs_BRANCH="bundle-2015-may"
 hos_docs_BRANCH="bundle-2015-may"
-carrier_grade_docs_BRANCH="TEST"
+carrier_grade_docs_BRANCH="test"
 public_cloud_docs_BRANCH="bundle-2015-may"
 hcf_docs_BRANCH="hcf-1.0-cf-release-certified"

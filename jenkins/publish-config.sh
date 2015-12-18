c346e1ed734dbf18622d374454fdf12aed928fc1
helion-jenkins <helion-jenkins@hp.com>
2015-12-18 19:42:49 +0000

    A jenkins job changed the hos publish branch to hos-20-21

--- jenkins/publish-config.sh
+++ jenkins/publish-config.sh
@@ -1,5 +1,5 @@
 devplat_docs_BRANCH="bundle-2015-may"
-hos_docs_BRANCH="bundle-2015-may"
+hos_docs_BRANCH="hos-20-21"
 carrier_grade_docs_BRANCH="bundle-2015-may branch"
 public_cloud_docs_BRANCH="bundle-2015-may"
 hcf_docs_BRANCH="hcf-0.10.0"

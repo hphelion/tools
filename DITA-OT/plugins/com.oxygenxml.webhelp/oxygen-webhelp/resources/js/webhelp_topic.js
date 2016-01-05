73c6e9ad6acfefdd0b0cd498dca5ae76e86b7d06
Nancymic2 <michell@fas.harvard.edu>
2016-01-05 11:20:26 -0500

    bad test

--- DITA-OT/plugins/com.oxygenxml.webhelp/oxygen-webhelp/resources/js/webhelp_topic.js
+++ DITA-OT/plugins/com.oxygenxml.webhelp/oxygen-webhelp/resources/js/webhelp_topic.js
@@ -142,7 +142,7 @@ $(document).ready(function () {
 	var testnode2=testnode.trim();
 	var firstchar = testnode2.charAt(0);
 	
-	if (firstchar=='<' && !testnode.search('http://')) {
+	if (firstchar=='<' ) {
           var linktext=lists[i].children[0].innerHTML;
           var hrf=lists[i].children[0].href;
           var hrf2=hrf.split('#')[1];   //target

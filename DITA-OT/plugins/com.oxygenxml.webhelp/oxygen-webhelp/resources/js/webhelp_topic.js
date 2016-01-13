cdb67021fdf8814fae1d989bae843e31ccd90f09
Nancymic2 <michell@fas.harvard.edu>
2016-01-13 07:46:32 -0500

    fix for euca

--- DITA-OT/plugins/com.oxygenxml.webhelp/oxygen-webhelp/resources/js/webhelp_topic.js
+++ DITA-OT/plugins/com.oxygenxml.webhelp/oxygen-webhelp/resources/js/webhelp_topic.js
@@ -136,7 +136,7 @@ $(document).ready(function () {
  var allAnchors=document.getElementsByTagName('A');
  for (var i=0; i<allAnchors.length; i++) {
      var hrf=allAnchors[i].href;
-     if (hrf.indexOf('#')!=-1) {
+     if (hrf.indexOf('#')!=-1 && hrf.search('http://')<0 ) {
         allAnchors[i].setAttribute('target', '_self');
      }
   }

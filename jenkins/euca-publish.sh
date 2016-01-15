325869c9656a60c197d3bac7acc94a594839b61f
Greg Larsen <greg.larsen@hpe.com>
2016-01-15 13:45:32 +0100

    refactoring for euca

--- jenkins/euca-publish.sh
+++ jenkins/euca-publish.sh
@@ -14,7 +14,7 @@ echo ">>Ensure target directory has correct ownership..."
 ssh ubuntu@${SERVER1} "sudo chown -R ubuntu:www-data /var/www/html/eucaProd/"
 
 echo ">>rsync /var/www/html/euca-incoming/staging  to /var/www/html/eucaProd/$VERSION_TO_PUBLISH "
-rsync -avr --delete   /var/www/html/euca-incoming/staging ubuntu@${SERVER1}:/var/www/html/eucaProd
+rsync -avr --delete   /var/www/html/euca-incoming/staging/* ubuntu@${SERVER1}:/var/www/html/eucaProd/$VERSION_TO_PUBLISH
 
 ssh ubuntu@${SERVER1} 'sudo chmod 777 /var/www/html/hphelionFransProd/.htaccess'
 

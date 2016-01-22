473ffdf7b12fa08a3dba340201dfb58a7191d9a7
Nancymic2 <michell@fas.harvard.edu>
2016-01-22 08:41:17 -0500

    HA code

--- DITA-OT/plugins/com.oxygenxml.webhelp/oxygen-webhelp/resources/js/webhelp_topic.js
+++ DITA-OT/plugins/com.oxygenxml.webhelp/oxygen-webhelp/resources/js/webhelp_topic.js
@@ -93,6 +93,67 @@ $(document).ready(function () {
 /*added 12-2-15 NM add event listeners to all sections of class headerH */
   $(document).ready(function()
       { 
+      	/////////////////begin HA////////////////////
+      	 if (document.getElementsByClassName('wrapper1')) {
+      	document.getElementById('HP2.0HA__desc').setAttribute('id', 'HP20HA__desc');
+      	document.getElementById('HP2.0HA__contentContainer').setAttribute('id', 'HP20HA__contentContainer');
+        var allids=document.getElementsByClassName('HAcontent');
+        allids[0].setAttribute('id', 'itemOne');
+        allids[1].setAttribute('id', 'itemTwo');
+        allids[2].setAttribute('id', 'itemThree');
+        allids[3].setAttribute('id', 'itemFour');
+        allids[4].setAttribute('id', 'itemFive');
+  
+        var allliitems=document.getElementsByClassName('itemLinks');
+        allliitems[0].setAttribute('data-pos', '0px');
+        allliitems[1].setAttribute('data-pos', '-1000px');
+        allliitems[2].setAttribute('data-pos', '-2000px');
+        allliitems[3].setAttribute('data-pos', '-3000px');
+        allliitems[4].setAttribute('data-pos', '-4000px');
+        var rappers=document.getElementsByClassName('wrapper1');
+        rappers[0].setAttribute('id', 'wrapper');
+        var mainlinks=document.getElementsByClassName('navLinks1');
+        mainlinks[0].setAttribute('id', 'navLinks');
+        var links = document.getElementsByClassName("itemLinks");
+var wrapper = document.getElementById("wrapper");
+
+// Build array of slide information.
+HAinfo = [
+			"The HPE Helion OpenStack installer deploys highly available configurations of OpenStack cloud services, resilient against single points of failure. Step through the included flow for an API request in an HA deployment. All API requests (internal and external) are sent through the VIP. ",
+			"keepalived has currently configured the VIP on the Controller0 node; client sends Nova request to VIP:8774",
+			"HA proxy (listening on VIP:8774) receives the request and selects Controller0 from the list of available nodes (Controller0, Controller1, Controller2). The request is forwarded to the Controller0IP:8774",
+			"nova-api on Controller0 receives the request and determines that a database change is required. It connects to the database using VIP:3306",
+			"HA proxy (listening on VIP:3306) receives the database connection request and selects Controller0 from the list of available nodes (Controller0, Controller1, Controller2). The connection request is forwarded to Controller0IP:3306",
+			];
+
+// Set initial text.
+document.getElementById("HP20HA__desc").innerHTML = HAinfo[0];
+
+// the activeLink provides a pointer to the currently displayed item
+var activeLink = 0;
+ 
+// setup the event listeners
+for (var i = 0; i < links.length; i++) {
+    var link = links[i];
+    link.addEventListener('click', setClickedItem, false);
+ 
+    // identify the item for the activeLink
+    link.itemID = i;
+}
+// set first item as active
+links[activeLink].classList.add("active");
+  
+    var transforms = ["transform",
+            "msTransform",
+            "webkitTransform",
+            "mozTransform",
+            "oTransform"];
+ 
+   transformPropertyHAHAHA = getSupportedPropertyName(transforms);
+
+}
+      ///////////////end HA ///////////////////////////	
+      	
           
         if (document.getElementsByClassName("headerH")){
 
@@ -143,7 +204,43 @@ $(document).ready(function () {
      }
   }
 }); 
-  
+  ///////////////HA//////////////////////
+  function setClickedItem(e) {
+    removeActiveLinks();
+ 
+    var clickedLink = e.target;
+    activeLink = clickedLink.itemID;
+ 
+    changePosition(clickedLink);
+	document.getElementById("HP20HA__desc").innerHTML = HAinfo[clickedLink.itemID];
+}
+ 
+function removeActiveLinks() {
+var links = document.getElementsByClassName("itemLinks");
+    for (var i = 0; i < links.length; i++) {
+        links[i].classList.remove("active");
+    }
+}
+ 
+// Handle changing the slider position as well as ensure
+// the correct link is highlighted as being active
+function changePosition(link) {
+    var position = link.getAttribute("data-pos");
+ 
+    var translateValue = "translate3d(" + position + ", 0px, 0)";
+    wrapper.style[transformPropertyHAHAHA] = translateValue;
+ 
+    link.classList.add("active");
+}
+function getSupportedPropertyName(properties) {
+    for (var i = 0; i < properties.length; i++) {
+        if (typeof document.body.style[properties[i]] != "undefined") {
+            return properties[i];
+        }
+    }
+    return null;
+}
+//////////////end HA ///////////////////
   //added anchor replacement 12/27/15 NM
    function anchorMove1() {
    	var bod=document.getElementsByTagName('body');

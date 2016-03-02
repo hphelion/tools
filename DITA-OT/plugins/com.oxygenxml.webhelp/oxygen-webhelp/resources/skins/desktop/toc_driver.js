9fce4096b6555f33cc3cdb279df79d8bb0287781
Gabriel McGoldrick <gabriel.mcgoldrick@hpe.com>
2016-03-02 21:35:03 +0000

    fix for scrolling

--- DITA-OT/plugins/com.oxygenxml.webhelp/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ DITA-OT/plugins/com.oxygenxml.webhelp/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -212,7 +212,9 @@ function loadIframe(dynamicURL) {
         $('#frm').show();
         debug('anchorID ' + anchorID);
         var selector = "#" + anchorID +"";
-        $(selector).get(0).scrollIntoView();
+        var el = $('#frm').contents().find(selector);
+        var elDOM = el.get(0)
+        elDOM.scrollIntoView();
         $('div.tooltip').remove();
         $('#breadcrumbLinks').find('a').after('<span>&nbsp;/&nbsp;</span>');
         $('#breadcrumbLinks').find('span').last().html('&nbsp;&nbsp;');

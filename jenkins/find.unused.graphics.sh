3e4080be4e3dd22239df576fa323bef91dd76ed2
Greg Larsen <greg.larsen@hpe.com>
2016-03-04 10:58:19 +0100

    update script

--- jenkins/find.unused.graphics.sh
+++ jenkins/find.unused.graphics.sh
@@ -5,24 +5,24 @@ cat temp.txt | sort | uniq > files.on.disk.txt
 
 for i in `find . -name "*.dita"`;
 do
-	egrep '(png|jpg|bmp|gif)' $i | sed 's|.* href=".*/\([^"]*\)".*|\1|g' | sed 's|.*src=".*/\([^"]*\)".*|\1|g' |\
-    grep -v "<" ;
-    done > temp.txt
-    cat temp.txt | sort | uniq > files.in.dita.txt
+	egrep '(png|jpg|bmp|gif)' $i | sed 's|.* href=".*/\([^"]*\)".*|\1|g' | sed 's|.*src=".*/\([^"]*\)".*|\1|g' | grep -v "<" ;
+done > temp.txt
 
-    if $(diff files.in.dita.txt files.on.disk.txt  | grep ">" | sed 's|>||g')
+cat temp.txt | sort | uniq > files.in.dita.txt
+
+if $(diff files.in.dita.txt files.on.disk.txt  | grep ">" | sed 's|>||g')
     then
 		echo "========================"
 		echo "No unused graphics found"
 		echo "========================"
 		exit 0;
-	else
-	    echo "================================================================================"
+    else
+		echo "================================================================================"
 		echo "Graphics files that are not referenced by any ditafile in this branch and repo"
 		for i in `diff files.in.dita.txt files.on.disk.txt  | grep ">" | sed 's|>||g'`
-		do
-			find . -name $i
-		done
+			do
+				find . -name $i
+			done
 		echo "================================================================================"
 		exit 1;
-done
+fi

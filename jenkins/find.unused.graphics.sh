ef3a387cd744d64b8b8752235fdc9b29136aae6f
Greg Larsen <greg.larsen@hpe.com>
2016-03-04 14:43:18 +0100

    bugfixes

--- jenkins/find.unused.graphics.sh
+++ jenkins/find.unused.graphics.sh
@@ -11,23 +11,23 @@ done > temp.txt
 
 cat temp.txt | sort | uniq > files.in.dita.txt
 
-curl -s https://raw.githubusercontent.com/hphelion/tools/master/jenkins/hipchat-notification.sh hipchat-notification.sh
 
 if [[ $(diff files.in.dita.txt files.on.disk.txt  | grep ">" | sed 's|>||g') ]]
     then
-		echo "================================================================================"
-		echo "Graphics files that are not referenced by any ditafile in this branch and repo"
-		for i in `diff files.in.dita.txt files.on.disk.txt  | grep ">" | sed 's|>||g'`
-			do
-				find . -name $i
-			done
-		echo "================================================================================"
-		exit 1;                
+                echo "================================================================================"
+                echo "Graphics files that are not referenced by any ditafile in this branch and repo"
+                for i in `diff files.in.dita.txt files.on.disk.txt  | grep ">" | sed 's|>||g'`
+                        do
+                                find . -name $i
+                        done
+                echo "================================================================================"
+				./hipchat-notification.sh green  145  "Unused graphics found in the $REPO_BRANCH_TO_CHECK branch of the $REPO repo" 1
+                exit 1;
     else
-		echo "========================"
-		echo "No unused graphics found"
-		echo "========================"
-	        ./hipchat-notification green  145  "No unused graphics found" 0	
-		exit 0;
+                echo "========================"
+                echo "No unused graphics found"
+                echo "========================"
+                ./hipchat-notification.sh green  145  "No unused graphics found" 0
+                exit 0;
 fi
 

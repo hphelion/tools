f8371665c80a72b963fef739173e87a7e0b8ea6e
helion-jenkins <helion-jenkins@hp.com>
2016-03-08 10:26:39 -0800

    A jenkins job changed the carrier.grade publish branch to bundle-2015-may

--- jenkins/publish-config.sh
+++ jenkins/publish-config.sh
@@ -1,6 +1,6 @@
 devplat_docs_BRANCH="master"
 hos_docs_BRANCH="hos-20-21"
-carrier_grade_docs_BRANCH="cg_published"
+carrier_grade_docs_BRANCH="bundle-2015-may"
 public_cloud_docs_BRANCH="bundle-2015-may"
 hcf_docs_BRANCH="hcf-0.10.0"
 hos_docs_LEGACYBRANCH="hos-30"

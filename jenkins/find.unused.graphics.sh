9b827171fe9eabad0687a91a21e3a51f69fa92d7
Greg Larsen <greg.larsen@hpe.com>
2016-03-22 16:24:25 +0100

    update find unused graphics

--- jenkins/find.unused.graphics.sh
+++ jenkins/find.unused.graphics.sh
@@ -7,7 +7,7 @@ REPO=$2
 find  . -name "*.png" -o -name "*.jpg" -o -name "*.bmp" -o -name "*.gif" | sed 's|.*/||g' > temp.txt
 cat temp.txt | sort | uniq > files.on.disk.txt
 
-for i in `find . -name "*.dita"`;
+for i in `find . -type f`;
 do
         egrep '(png|jpg|bmp|gif)' $i | sed 's|.* href=".*/\([^"]*\)".*|\1|g' | sed 's|.*src=".*/\([^"]*\)".*|\1|g' | grep -v "<" ;
 done > temp.txt
@@ -18,7 +18,7 @@ cat temp.txt | sort | uniq > files.in.dita.txt
 if [[ $(diff files.in.dita.txt files.on.disk.txt  | grep ">" | sed 's|>||g') ]]
     then
                 echo "================================================================================"
-                echo "Graphics files that are not referenced by any ditafile in this branch and repo"
+                echo "Graphics files that are not referenced by any file in this branch and repo"
                 for i in `diff files.in.dita.txt files.on.disk.txt  | grep ">" | sed 's|>||g'`
                         do
                                 find . -name $i

92603ad1f1d7ef835a48572c7c40d4e1be685ba1
Nancymic2 <nancy.ann.michell@hpe.com>
2016-03-31 17:04:33 -0400

    prettyprint

--- DITA-OT/plugins/com.oxygenxml.webhelp/oxygen-webhelp/resources/js/webhelp_topic.js
+++ DITA-OT/plugins/com.oxygenxml.webhelp/oxygen-webhelp/resources/js/webhelp_topic.js
@@ -103,8 +103,10 @@ $(document).ready(function () {
       	$.getScript("http://docs-staging.hpcloud.com/z_test_hos.docs+hos-30+nancy/media/run_prettify.js");
       	var pretags=document.getElementsByTagName("PRE");
       	for (var q=0; q<pretags.length; q++) {
-      		//pretags[q].setAttribute('class', 'pre codeblock prettyprint');
-      		pretags[q].className+=" prettyprint";
+      		var currclass=pretags[q].className;
+      		var newclass=currclass + " prettyprint";
+      		pretags[q].setAttribute('class', newclass);
+      		//pretags[q].className+=" prettyprint";
       	}
 /* ------------------------begin HA-----------------------------------------------------------------*/
       	 if (document.getElementsByClassName('wrapper1').length) {

de671ca44103b9844ce11a14f8fbf05ba578085d
Greg Larsen <greg.larsen@hpe.com>
2016-06-10 14:27:45 +0200

    updates for time stamp

--- jenkins/inject_date.sh
+++ jenkins/inject_date.sh
@@ -34,7 +34,8 @@ do
     sed -i "s|<\/h1>|</h1><p class=\"heliondate\">Last updated: $PRETTYDATE<a href=\"\" class="xref" style=\"float:right\" onclick=\"window.print()\">Print this page</a> </p>|" $fullpath    
 	
 	
+	stat --format=%y   $fullpath 
 	touch -d "$PRETTYDATE"  $fullpath 
-
+     stat --format=%y   $fullpath 
 done
 echo ===end inject_date.sh===

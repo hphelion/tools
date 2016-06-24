ad1cf1f880337e325d6583313eff43a082e99c24
Nancymic2 <nancy.ann.michell@hpe.com>
2016-06-24 13:12:49 -0400

    script

--- DITA-OT/plugins/com.oxygenxml.webhelp/oxygen-webhelp/resources/js/webhelp_topic.js
+++ DITA-OT/plugins/com.oxygenxml.webhelp/oxygen-webhelp/resources/js/webhelp_topic.js
@@ -93,7 +93,7 @@ $(document).ready(function () {
 /*added 12-2-15 NM add event listeners to all sections of class headerH */
   $(document).ready(function()
     { 
-    	   $.getScript("http://docs-staging.hpcloud.com/hos.docs-hos-30/oxygen-webhelp/resources/js/networkimages.js");
+    	   $.getScript("http://docs.hpcloud.com/oxygen-webhelp/resources/js/networkimages.js");
 
     	/*------------------network diagrams-------------------------------*/
   	

1dc24d21b4d8e272018858f567623207fa134ffd
Greg Larsen <greg.larsen@hpe.com>
2016-08-04 19:25:48 +0200

    improvements

--- jenkins/functionLibrary.sh
+++ jenkins/functionLibrary.sh
@@ -488,4 +488,23 @@ function inject_redirects () {
 		sed -i "s|function loadIframe(dynamicURL) {|function loadIframe(dynamicURL) { $REDIRECT |"  ./out/webhelp/oxygen-webhelp/resources/skins/desktop/toc_driver.js
 	done < inter-helpset-redirects.tmp 
 
-echo ===end inject_redirects===}
+	echo ===end inject_redirects===
+}
+
+
+
+
+function license () {
+echo "------START-LICENSE-KEY------
+Registration_Name=Eucalyptus Systems
+Company=Eucalyptus Systems
+Category=Enterprise-Floating
+Component=Scripting
+Version=15
+Number_of_Licenses=1
+Date=11-08-2013
+Maintenance=0
+SGN=MCwCFDDNusJoEVUc9F8j3jbCgNofpljwAhQVGwO5WPSaMVLfmtXLIlZxFMJ99w\=\=
+-------END-LICENSE-KEY-------
+" > ./tools/DITA-OT/plugins/com.oxygenxml.webhelp/licensekey.txt
+}

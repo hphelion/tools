9e7fcd6ebead4e6058f2fcadcbc3269d3b53c604
Greg Larsen <greg.larsen@hpe.com>
2016-08-04 19:00:18 +0200

    improvements

--- jenkins/functionLibrary.sh
+++ jenkins/functionLibrary.sh
@@ -460,3 +460,19 @@ function inject_date () {
 
 
 }
+
+
+function inject_disclaimer () {
+
+	echo ===start inject_disclaimer===
+	DISCLAIMER=`cat disclaimer_snippet` || true
+
+	for i in `find ./out/webhelp -name "*.html"`
+	do
+		echo "inject disclaimer into $i"
+		sed -i "s|\([^>]\)</h1>|\1</h1> $DISCLAIMER|g" $i
+	done
+
+echo ===stop inject_disclaimer===
+
+}

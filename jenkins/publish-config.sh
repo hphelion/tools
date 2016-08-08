a3a7284d4dfe2df25b7514712c02f8cf11bdda10
Greg Larsen <greg.larsen@hpe.com>
2016-08-08 13:11:40 +0200

    testing

--- jenkins/publish-config.sh
+++ jenkins/publish-config.sh
@@ -1,7 +1,5 @@
 devplat_docs_BRANCH="master"
 hos_docs_BRANCH="hos-30"
-
 public_cloud_docs_BRANCH="bundle-2015-may"
 hcf_docs_BRANCH="hcf-0.10.0"
 hos_docs_LEGACYBRANCH="hos-20-21"
-s|carrier_grade_docs_BRANCH.*|carrier_grade_docs_BRANCH="greg-test"|

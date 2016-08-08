4c284acedac15cfcb111578475b6b80f68f4920a
helion-jenkins <helion-jenkins@hp.com>
2016-08-08 04:14:09 -0700

    A jenkins job changed the carrier.grade publish branch to greg-test

--- jenkins/publish-config.sh
+++ jenkins/publish-config.sh
@@ -3,4 +3,5 @@ hos_docs_BRANCH="hos-30"
 public_cloud_docs_BRANCH="bundle-2015-may"
 hcf_docs_BRANCH="hcf-0.10.0"
 hos_docs_LEGACYBRANCH="hos-20-21"
-carrier_grade_docs_BRANCH="cg_published"
+
+carrier_grade_docs_BRANCH="greg-test"

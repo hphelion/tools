180cccdbc2567df185a9ab7873b8d993b4551339
Greg Larsen <greg.larsen@hpe.com>
2016-08-09 17:06:30 +0200

    updates to functionLibrary.sh

--- jenkins/functionLibrary.sh
+++ jenkins/functionLibrary.sh
@@ -16,7 +16,7 @@ TEST_LAN_IP="192.168.251.17" 		#Internal IP address for docs-staging.hpcloud.com
 
 get_the_tools_repo () {
 echo "
->>>> Startgin ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
+>>>> Starting ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
 	
 	#If no argument was passed to the function, use the master branch.  Otherwise use the argument as the branch
 	if [[ -z "$1"   ]];
@@ -49,7 +49,7 @@ echo "
 
 function adjust_date_to_last_commit {
 echo "
->>>> Startgin ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
+>>>> Starting ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
 	
 	#Note that this only works on a complete repo.  A shallow clone does not have all the needed info.
 	
@@ -80,7 +80,7 @@ echo "
  
  function clone_repo {
 echo "
->>>> Startgin ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
+>>>> Starting ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
 	
 	#Set branch and repo variables from the function's arguments
 	repo=$1
@@ -117,7 +117,7 @@ echo "
 
  function extractBranch () {
 echo "
->>>> Startgin ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
+>>>> Starting ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
 	#Extract the branch from the a string taken from $HUDSON_HOME/doc-build-resources/repos+branches.txt
 	echo "$1" | sed 's|\([^ ]*\).*$|\1|'
  echo ">>> END ${FUNCNAME[0]}
@@ -127,7 +127,7 @@ echo "
 
  function extractRepo () {
 echo "
->>>> Startgin ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
+>>>> Starting ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
 
 	#Extract the repo from the a string taken from $HUDSON_HOME/doc-build-resources/repos+branches.txt
 	echo "$1" | sed 's|.*of the \([^ ]*\) repo)|\1|'
@@ -139,7 +139,7 @@ echo "
 
 function hipChat () {
 echo "
->>>> Startgin ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
+>>>> Starting ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
 
 #Usage: hipChat (PASS|FAIL) "MESSAGE" ROOM
 #Set the URL to the console output for this build
@@ -205,7 +205,7 @@ done
 
 function build.on.push () {
 echo "
->>>> Startgin ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
+>>>> Starting ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
 
 
 #Set the HipChat rooms to notify in case of a pass or fail of this build
@@ -291,7 +291,7 @@ sudo cp /var/lib/jenkins/HPE-Helion.png ./out/webhelp/
 
 function insert_disclaimer () {
 echo "
->>>> Startgin ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
+>>>> Starting ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
 
 	DISCLAIMER=`cat disclaimer_snippet` || true
 	for i in `find ./out/webhelp -name "*.html"`
@@ -307,7 +307,7 @@ echo "
 
 function assemble_repos () {
 echo "
->>>> Startgin ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
+>>>> Starting ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
 
 
 #Set the variable to whatever was passed to this script.
@@ -429,7 +429,7 @@ function production_build () {
 	#NOTE: Call assemble-repos before running
 	
 echo "
->>>> Startgin ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
+>>>> Starting ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
 
  
 
@@ -460,7 +460,7 @@ echo "
 
 function inject_date () {
 echo "
->>>> Startgin ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
+>>>> Starting ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
 
 	for i in `find  -name "*.dita" -not -path "./publiccloud/api/*"`
 	do
@@ -509,7 +509,7 @@ echo ">>> END ${FUNCNAME[0]}
 
 function inject_disclaimer () {
 echo "
->>>> Startgin ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
+>>>> Starting ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
 
 	
 	DISCLAIMER=`cat disclaimer_snippet` || true
@@ -528,7 +528,7 @@ echo "
 
 function inject_redirects () {
 echo "
->>>> Startgin ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
+>>>> Starting ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
 
 
 	grep -v "^#" ./tools/jenkins/inter-helpset-redirects.txt > inter-helpset-redirects.tmp 
@@ -547,7 +547,7 @@ echo "
 
 function license () {
 echo "
->>>> Startgin ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
+>>>> Starting ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
 
 echo "------START-LICENSE-KEY------
 Registration_Name=Eucalyptus Systems
@@ -570,7 +570,7 @@ SGN=MCwCFDDNusJoEVUc9F8j3jbCgNofpljwAhQVGwO5WPSaMVLfmtXLIlZxFMJ99w\=\=
 
 oxygen-webhelp-build () {
 echo "
->>>> Startgin ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
+>>>> Starting ${FUNCNAME[0]} (referenced from functionLibrary.sh)"
 
 
 if [ -z "$1" ]

afefc9846899483ca570796b5fd0e39161ca2593
Carter A. Thompson <carter.thompson@hp.com>
2016-08-17 15:31:59 -0700

    Added 'hrc_docs_BRANCH="master"' in the publish-config.sh file.

--- jenkins/publish-config.sh
+++ jenkins/publish-config.sh
@@ -3,6 +3,7 @@ hos_docs_BRANCH="hos-30"
 public_cloud_docs_BRANCH="bundle-2015-may"
 hcf_docs_BRANCH="hcf-0.10.0"
 hos_docs_LEGACYBRANCH="hos-20-21"
+hrc_docs_BRANCH="master"
 
 
 carrier_grade_docs_BRANCH="cg_published"

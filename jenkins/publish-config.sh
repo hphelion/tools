655cf91a016a40f4276a55dca96076198b7bf17e
Greg Larsen <greg.larsen@hpe.com>
2016-10-21 07:31:17 +0200

    add artifacts for stackato publishing

--- jenkins/publish-config.sh
+++ jenkins/publish-config.sh
@@ -4,6 +4,5 @@ public_cloud_docs_BRANCH="bundle-2015-may"
 hcf_docs_BRANCH="hcf-0.10.0"
 hos_docs_LEGACYBRANCH="hos-20-21"
 hrc_docs_BRANCH="master"
-
-
+stackato_docs_BRANCH="master"
 carrier_grade_docs_BRANCH="cg_published"

fb0359fd55c398b5ae657fac6c51f0fcedebd390
Gabriel McGoldrick <gabriel.mcgoldrick@hpe.com>
2016-11-10 16:24:30 +0000

    fix broken js for images in entry scale articls

--- DITA-OT/plugins/com.oxygenxml.webhelp/oxygen-webhelp/resources/js/entryScale.js
+++ DITA-OT/plugins/com.oxygenxml.webhelp/oxygen-webhelp/resources/js/entryScale.js
@@ -1,14 +1,14 @@
          netcounter=0;
          netimages = [
 
-    'http://docs.hpcloud.com/3.x/media/entryScale/Entry-ScaleExternalVM.png',
-    'http://docs.hpcloud.com/3.x/media/entryScale/Entry-ScaleExternalAPI.png',
-    'http://docs.hpcloud.com/3.x/media/entryScale/Entry-ScaleGuest.png',
-    'http://docs.hpcloud.com/3.x/media/entryScale/Entry-ScaleProviderVLANS.png',
-    'http://docs.hpcloud.com/3.x/media/entryScale/Entry-ScaleOctavia.png',
-    'http://docs.hpcloud.com/3.x/media/entryScale/Entry-ScaleManagement.png',
-    'http://docs.hpcloud.com/3.x/media/entryScale/Entry-ScaleAllNetworks.png'];
+    'https://docs.hpcloud.com/hos-4.x/media/entryScale/Entry-ScaleExternalVM.png',
+    'https://docs.hpcloud.com/hos-4.x/media/entryScale/Entry-ScaleExternalAPI.png',
+    'https://docs.hpcloud.com/hos-4.x/media/entryScale/Entry-ScaleGuest.png',
+    'https://docs.hpcloud.com/hos-4.x/media/entryScale/Entry-ScaleProviderVLANS.png',
+    'https://docs.hpcloud.com/hos-4.x/media/entryScale/Entry-ScaleOctavia.png',
+    'https://docs.hpcloud.com/hos-4.x/media/entryScale/Entry-ScaleManagement.png',
+    'https://docs.hpcloud.com/hos-4.x/media/entryScale/Entry-ScaleAllNetworks.png'];
 
 var getnetworks=document.getElementsByClassName('entryScale');
 
-getnetworks[0].innerHTML='<div id="allmaps" oncontextmenu="noclick(); return false;"><img id="solid" src="http://docs.hpcloud.com/3.x/media/entryScale/Entry-ScaleAllNetworks.png"><img id="all2" src="http://docs.hpcloud.com/3.x/media/entryScale/Entry-Scale-2pxBorder.png"  border="0"<img id="entryScale" src="http://docs.hpcloud.com/3.x/media/entryScale/Entry-Scale-2pxBorder.png" border="0" width="1000" height="900" orgWidth="1000" orgHeight="900" usemap="#entryScale" alt="" /><map name="entryScale"><area  alt="" title="EXTERNAL-VM" href="#" shape="rect" coords="13,63,130,86" style="outline:none;" onclick="changeimg(0);"     /><area  alt="" title="EXTERNAL-API" href="#" shape="rect" coords="13,91,137,114" style="outline:none;" onclick="changeimg(1);"     /><area  alt="" title="GUEST" href="#" shape="rect" coords="5,141,129,164" style="outline:none;" onclick="changeimg(2);"     /><area  alt="" title="Provider VLANs" href="#" shape="rect" coords="5,167,129,190" style="outline:none;" onclick="changeimg(3);"     /><area  alt="" title="OCTAVIA-MGMT" href="#" shape="rect" coords="7,191,131,210" style="outline:none;" onclick="changeimg(4);"     /><area  alt="" title="MANAGEMENT" href="#" shape="rect" coords="8,211,132,233" style="outline:none;" onclick="changeimg(5);"     /><area  alt="" title="All Networks" href="#" shape="rect" coords="5,234,130,256" style="outline:none;" onclick="changeimg(6);"     /></map>';
+getnetworks[0].innerHTML='<div id="allmaps" oncontextmenu="noclick(); return false;"><img id="solid" src="https://docs.hpcloud.com/hos-4.x/media/entryScale/Entry-ScaleAllNetworks.png"><img id="all2" src="https://docs.hpcloud.com/hos-4.x/media/entryScale/Entry-Scale-2pxBorder.png"  border="0"<img id="entryScale" src="https://docs.hpcloud.com/hos-4.x/media/entryScale/Entry-Scale-2pxBorder.png" border="0" width="1000" height="900" orgWidth="1000" orgHeight="900" usemap="#entryScale" alt="" /><map name="entryScale"><area  alt="" title="EXTERNAL-VM" href="#" shape="rect" coords="13,63,130,86" style="outline:none;" onclick="changeimg(0);"     /><area  alt="" title="EXTERNAL-API" href="#" shape="rect" coords="13,91,137,114" style="outline:none;" onclick="changeimg(1);"     /><area  alt="" title="GUEST" href="#" shape="rect" coords="5,141,129,164" style="outline:none;" onclick="changeimg(2);"     /><area  alt="" title="Provider VLANs" href="#" shape="rect" coords="5,167,129,190" style="outline:none;" onclick="changeimg(3);"     /><area  alt="" title="OCTAVIA-MGMT" href="#" shape="rect" coords="7,191,131,210" style="outline:none;" onclick="changeimg(4);"     /><area  alt="" title="MANAGEMENT" href="#" shape="rect" coords="8,211,132,233" style="outline:none;" onclick="changeimg(5);"     /><area  alt="" title="All Networks" href="#" shape="rect" coords="5,234,130,256" style="outline:none;" onclick="changeimg(6);"     /></map>';
--- DITA-OT/plugins/com.oxygenxml.webhelp/oxygen-webhelp/resources/js/entryScaleDedicated.js
+++ DITA-OT/plugins/com.oxygenxml.webhelp/oxygen-webhelp/resources/js/entryScaleDedicated.js
@@ -2,14 +2,14 @@
          netcounter=0;
          netimages = [
 
-    'http://docs.hpcloud.com/3.x/media/entryScaleDed/Entry-ScaleDedicatedExternalVM.png',
-    'http://docs.hpcloud.com/3.x/media/entryScaleDed/Entry-ScaleDedicatedExternalAPI.png',
-    'http://docs.hpcloud.com/3.x/media/entryScaleDed/Entry-ScaleDedicatedGuest.png',
-    'http://docs.hpcloud.com/3.x/media/entryScaleDed/Entry-ScaleDedicatedProviderVLANS.png',
-    'http://docs.hpcloud.com/3.x/media/entryScaleDed/Entry-ScaleDedicatedOctavia.png',
-    'http://docs.hpcloud.com/3.x/media/entryScaleDed/Entry-ScaleDedicatedManagement.png',
-    'http://docs.hpcloud.com/3.x/media/entryScaleDed/Entry-ScaleDedicatedAllNetworks.png'];
+    'https://docs.hpcloud.com/hos-4.x/media/entryScaleDed/Entry-ScaleDedicatedExternalVM.png',
+    'https://docs.hpcloud.com/hos-4.x/media/entryScaleDed/Entry-ScaleDedicatedExternalAPI.png',
+    'https://docs.hpcloud.com/hos-4.x/media/entryScaleDed/Entry-ScaleDedicatedGuest.png',
+    'https://docs.hpcloud.com/hos-4.x/media/entryScaleDed/Entry-ScaleDedicatedProviderVLANS.png',
+    'https://docs.hpcloud.com/hos-4.x/media/entryScaleDed/Entry-ScaleDedicatedOctavia.png',
+    'https://docs.hpcloud.com/hos-4.x/media/entryScaleDed/Entry-ScaleDedicatedManagement.png',
+    'https://docs.hpcloud.com/hos-4.x/media/entryScaleDed/Entry-ScaleDedicatedAllNetworks.png'];
 
 var getnetworks=document.getElementsByClassName('entryScaleDedicated');
 
-getnetworks[0].innerHTML='<div id="allmaps" oncontextmenu="noclick(); return false;"><img id="solid" src="http://docs.hpcloud.com/3.x/media/entryScaleDed/Entry-ScaleDedicatedAllNetworks.png"><img id="all2" src="http://docs.hpcloud.com/3.x/media/entryScaleDed/Entry-ScaleDedicated-2pxBorder.png"  border="0"<img id="entryScaleDedicated" src="http://docs.hpcloud.com/3.x/media/entryScaleDed/Entry-ScaleDedicated-2pxBorder.png" border="0" width="1000" height="900" orgWidth="1000" orgHeight="900" usemap="#entryScaleDedicated" alt="" /><map name="entryScaleDedicated"><area  alt="" title="EXTERNAL-VM" href="#" shape="rect" coords="14,73,150,87" style="outline:none;"  onclick="changeimg(0);"      /><area  alt="" title="EXTERNAL-API" href="#" shape="rect" coords="10,94,146,108" style="outline:none;"  onclick="changeimg(1);"      /><area  alt="" title="GUEST" href="#" shape="rect" coords="11,149,140,167" style="outline:none;"  onclick="changeimg(2);"      /><area  alt="" title="Provider VALNS" href="#" shape="rect" coords="0,170,140,188" style="outline:none;"  onclick="changeimg(3);"      /><area  alt="" title="OCTAVIA-MGMT" href="#" shape="rect" coords="0,190,133,206" style="outline:none;"  onclick="changeimg(4);"      /><area  alt="" title="MANAGEMENT" href="#" shape="rect" coords="4,211,140,229" style="outline:none;"  onclick="changeimg(5);"      /><area  alt="" title="All Networks" href="#" shape="rect" coords="3,232,140,254" style="outline:none;"  onclick="changeimg(6);"      /></map>';
+getnetworks[0].innerHTML='<div id="allmaps" oncontextmenu="noclick(); return false;"><img id="solid" src="https://docs.hpcloud.com/hos-4.x/media/entryScaleDed/Entry-ScaleDedicatedAllNetworks.png"><img id="all2" src="https://docs.hpcloud.com/hos-4.x/media/entryScaleDed/Entry-ScaleDedicated-2pxBorder.png"  border="0"<img id="entryScaleDedicated" src="https://docs.hpcloud.com/hos-4.x/media/entryScaleDed/Entry-ScaleDedicated-2pxBorder.png" border="0" width="1000" height="900" orgWidth="1000" orgHeight="900" usemap="#entryScaleDedicated" alt="" /><map name="entryScaleDedicated"><area  alt="" title="EXTERNAL-VM" href="#" shape="rect" coords="14,73,150,87" style="outline:none;"  onclick="changeimg(0);"      /><area  alt="" title="EXTERNAL-API" href="#" shape="rect" coords="10,94,146,108" style="outline:none;"  onclick="changeimg(1);"      /><area  alt="" title="GUEST" href="#" shape="rect" coords="11,149,140,167" style="outline:none;"  onclick="changeimg(2);"      /><area  alt="" title="Provider VALNS" href="#" shape="rect" coords="0,170,140,188" style="outline:none;"  onclick="changeimg(3);"      /><area  alt="" title="OCTAVIA-MGMT" href="#" shape="rect" coords="0,190,133,206" style="outline:none;"  onclick="changeimg(4);"      /><area  alt="" title="MANAGEMENT" href="#" shape="rect" coords="4,211,140,229" style="outline:none;"  onclick="changeimg(5);"      /><area  alt="" title="All Networks" href="#" shape="rect" coords="3,232,140,254" style="outline:none;"  onclick="changeimg(6);"      /></map>';

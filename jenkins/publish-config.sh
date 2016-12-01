b97778c3c3a181b04e671f7dac2b50da1078d67a
Carter A. Thompson <carter.thompson@hpe.com>
2016-12-01 15:25:44 -0800

    Reverting back the hos_docs_BRANCH=“hos-30”

--- jenkins/publish-config.sh
+++ jenkins/publish-config.sh
@@ -1,5 +1,5 @@
 devplat_docs_BRANCH="master"
-hos_docs_BRANCH="hos-40"
+hos_docs_BRANCH="hos-30"
 public_cloud_docs_BRANCH="bundle-2015-may"
 hcf_docs_BRANCH="hcf-0.10.0"
 hos_docs_LEGACYBRANCH="hos-20-21"
